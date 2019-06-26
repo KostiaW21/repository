6783a50b008ae97fadce0c8ebea7c4d52913aea4
_Kostia_ <kostia902@ukr.net>
2019-06-26 12:49:13 +0300

    Kostia
    Added connection

--- API/services.php
+++ API/services.php
@@ -1,20 +1,30 @@
 <?php
 include 'connect.php';
 
-class Services{
+class ServicesTable{
 	
-	public function return_all_record()
+	public function getAll()
    {
-   			$sql = "SELECT * FROM services";
-   			$db = Database::getConnection();
-   			$result = $db->query($sql);
-			$json_str = json_encode($result->fetchAll(PDO::FETCH_ASSOC));
-			print_r($json_str);
+   		$sql = "SELECT * FROM services";
+   		$db = Database::getConnection();
+   		$result = $db->query($sql);
+		$json_str = json_encode($result->fetchAll(PDO::FETCH_ASSOC));
+		print_r($json_str);
    }
-  
+    public function add($serviceName)
+    {
+        $sql = "INSERT INTO services VALUES(:serviceName)";
+       	$db = Database::getConnection();
+        $result = $db->prepare($sql);
+        $result->bindParam(':serviceName',$serviceName,PDO::PARAM_STR,100);
+	    $result->execute();
+    }
+
 }
-$con = new Services();
-$con->return_all_record();
+
+$con = new ServicesTable();
+$con->add("Text");
+$con->getAll();
 ?>
 
 
